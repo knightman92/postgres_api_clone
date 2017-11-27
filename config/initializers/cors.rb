4bffe9ea9fa9863dad32df360a230ee393276b03
Kyle <krj221@knights.ucf.edu>
2017-11-27 16:36:25 -0500

    changed notification message

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200', 'http://test.offcourtapp.com', 'http://offcourtapp.com', 'https://d3kd393r0oekr.cloudfront.net'
+    # origins 'http://localhost:4200', 'http://test.offcourtapp.com', 'http://offcourtapp.com', 'https://d3kd393r0oekr.cloudfront.net'
+    origins '*'
 
-    resource '*',
+    endresource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
