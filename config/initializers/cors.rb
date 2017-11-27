dfef3e19ff47da51bbb6ceb1b364b04ab99467fc
Kyle <krj221@knights.ucf.edu>
2017-11-27 17:28:21 -0500

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'http://localhost:4200', 'http://test.offcourtapp.com', 'http://offcourtapp.com', 'https://d3kd393r0oekr.cloudfront.net'
-    origins '*'
+    origins 'http://localhost:4200', 'http://test.offcourtapp.com', 'http://offcourtapp.com', 'https://d3kd393r0oekr.cloudfront.net'
+    # origins '*'
 
     resource '*',
       headers: :any,
